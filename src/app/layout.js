60da058fdb65e33a493e3a3988fd8c756f98e934
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-06-30 11:37:51 +0530

    now remove all next js error

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import  "bootstrap/dist/css/bootstrap.min.css"
@@ -12,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid  d-flex justify-content-between">
          
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,6 +8,7 @@ import UpperHero from '../svg/UpperHero'
 import JoinHero from '../svg/Join';
 import Or from '../svg/OrConnect';
 import { usePathname } from 'next/navigation';
+import Login from './../../components/Login';
 
 const NavLinks = [
   { id: 1, name: 'join', path: '/join' },
@@ -28,75 +29,14 @@ const page = () => {
     });
     console.log("Signup success", response.data);
     router.push("/");
-    alert(`email ${name}`);
-    alert(`email ${passWord}`);
+   
     setName('')
     setPassWord('')
   }
   return (
 
     <>
-      <div className='container' >
-        <div className='row'>
-          <div className='col-8 container'>
-            <UpperHero />
-            <div className='container'>
-              <Hero />
-            </div>
-          </div>
-          <div className='col-4 d-flex flex-column'>
-            <div className='d-flex flex-row'>
-
-            </div>
-            <div className='d-flex flex-column'>
-              <div className='d-flex flex-column'>
-                <div className='d-flex flex-column'>
-                  <div className='d-flex justify-content-start w-100'>
-                <div className='d-flex justify-content-between w-50'>
-                <Link
-                      className = {
-                        isActive ? `text-decoration-none `:`text-decoration-none`
-                      
-                      }
-
-                      href='/login'>login</Link>
-                  <Link
-                      className = {
-                        isActive ? `text-decoration-none `:`text-decoration-non`
-                      
-                      }
-
-                      href='/join'>join</Link>
-                </div>
-                  </div>
-                  <div className='container flex-column'>
-                    <JoinHero />
-                    <Or />
-                  </div>
-                </div>
-
-
-                <form action="">
-                  <div
-                  className='d-flex flex-column'>
-                    <input type="email" className='rounded my-2' placeholder='email' value={name} onChange={(e) => {
-                    setName(e.target.value)
-                  }} />
-                  <input type="text" className='rounded my-2' placeholder='password' value={passWord} onChange={(e) => {
-                    setPassWord(e.target.value)
-                  }} />
-                  <button type="button" className="btn btn-primary rounded my-4" onClick={() => {
-                    return submitHandler()
-                  }}>Agree and continue</button>
-                  </div>
-                </form>
-              </div>
-
-            </div>
-          </div>
-        </div>
-
-      </div>
+      <Login/>
     </>
   )
 }
